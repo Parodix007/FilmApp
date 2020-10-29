f936c9b638a77f8044ead99944dbd404ddb0c4af
Parodix007 <sebastian.siarczynski@gmail.com>
2020-10-29 13:51:48 +0100

    Dodanie struktury html do projektu

--- FilmAppConfig/FilmApp/views.py
+++ FilmAppConfig/FilmApp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views import View
+
 
 # Create your views here.
+
+
+class MainView(View):
+    template = "MainPage.html"
+
+    def get(self, request):
+        return render(request, self.template, {"author": "Sebastian Siarczyński"})
