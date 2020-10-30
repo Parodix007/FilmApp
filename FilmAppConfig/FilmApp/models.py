eba183f8ca65f0b8216a73ce40cf79f160cdca93
Parodix007 <sebastian.siarczynski@gmail.com>
2020-10-30 13:59:57 +0100

    Stylizacja i pobieranie danych z bazy danych, przygotowywanie stylow strony

--- FilmAppConfig/FilmApp/models.py
+++ FilmAppConfig/FilmApp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Film(models.Model):
+    id = models.IntegerField(primary_key=True)
+    film_name = models.CharField(max_length=50)
+
+    def str(self):
+        return self.film_name
+
+
+class Genre(models.Model):
+    id = models.IntegerField(primary_key=True)
+    genre_name = models.CharField(max_length=50)
+
+    def str(self):
+        return self.genre_name
--- FilmAppConfig/FilmApp/views.py
+++ FilmAppConfig/FilmApp/views.py
@@ -1,17 +1,37 @@
 from django.shortcuts import render
 from django.views import View
 from django.db import connection
+from .models import Film, Genre
 
 # Create your views here.
 
 
 class MainView(View):
-    template = "MainPage.html"
+    template = "base.html"
 
-    with connection.cursor() as cursor:
-        cursor.execute("SELECT film_title FROM film")
-        film = cursor.fetchone()
-        print(film)
+    query_for_films = "SELECT id, film_title FROM film LIMIT 3"
+    query_for_genres = "SELECT id, genre_name FROM genre LIMIT 3"
+
+    list_with_films = []
+    list_with_genres = []
+
+    for item in Film.objects.raw(query_for_films):
+        list_with_films.append({"id": f"{item.id}", "title": f"{item.film_title}"})
+
+    for item in Genre.objects.raw(query_for_genres):
+        list_with_genres.append({"id": f"{item.id}", "name": f"{item.genre_name}"})
+    # with connection.cursor() as cursor:
+    #    cursor.execute("SELECT film_title FROM film")
+    #    film = cursor.fetchone()
+    #    print(film)
 
     def get(self, request):
-        return render(request, self.template, {"author": "Sebastian Siarczyński"})
+        return render(
+            request,
+            self.template,
+            {
+                "author": "Sebastian Siarczyński",
+                "films": self.list_with_films,
+                "genres": self.list_with_genres,
+            },
+        )
