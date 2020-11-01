efb8cdcee712722bdd15a215b246bc87ba34172b
Parodix007 <sebastian.siarczynski@gmail.com>
2020-11-01 01:20:26 +0100

    Dodanie blockow w html, zapewne zmieni sie to na include, w views dodanie pobranych danych z klasy do zapytan do bazy danych

--- FilmAppConfig/FilmApp/views.py
+++ FilmAppConfig/FilmApp/views.py
@@ -13,20 +13,11 @@ class MainView(View):
     query_for_films = "SELECT id, film_title FROM film LIMIT 3"
     query_for_genres = "SELECT id, genre_name FROM genre LIMIT 3"
 
-    query_for_films = GetDataFromDB(query_for_films)
-    list_with_films = query_for_films.get_film()
-    print(list_with_films)
+    getFilms = GetDataFromDB(query_for_films)
+    list_with_films = getFilms.get_film()
 
-    # for item in Film.objects.raw(query_for_films):
-    #     list_with_films.append({"id": f"{item.id}", "title": f"{item.film_title}"})
-    #
-    # for item in Genre.objects.raw(query_for_genres):
-    #     list_with_genres.append({"id": f"{item.id}", "name": f"{item.genre_name}"})
-
-    # with connection.cursor() as cursor:
-    #    cursor.execute("SELECT film_title FROM film")
-    #    film = cursor.fetchone()
-    #    print(film)
+    getGenres = GetDataFromDB(query_for_genres)
+    list_with_genres = getGenres.get_genre()
 
     def get(self, request):
         return render(
@@ -34,8 +25,8 @@ class MainView(View):
             self.template,
             {
                 "author": "Sebastian Siarczyński",
-                # "films": self.list_with_films,
-                # "genres": self.list_with_genres,
+                "films": self.list_with_films,
+                "genres": self.list_with_genres,
             },
         )
 
